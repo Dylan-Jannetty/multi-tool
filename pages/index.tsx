36c11c489acb372913a47e9a45d504f9641034c8
Dylan Jannetty <dylanjannetty@dylans-mbp.mynetworksettings.com>
2022-04-06 00:29:21 -0400

    add logout functionality with firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
+import { signOut } from "firebase/auth";
 import AuthPage from "../components/auth";
 import { useAuthValue } from "../context/AuthContext";
+import { auth } from "../firebase";
 
 export default function Home() {
   const currentUser = useAuthValue();
+
+  function handleLogOut(e) {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        console.log("you are logged out");
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
   return (
     <>
       {currentUser ? (
         <>
           <div>Hello World {currentUser.email}!</div>
+          <button onClick={(e) => handleLogOut(e)}>Log Out</button>
         </>
       ) : (
         <>
